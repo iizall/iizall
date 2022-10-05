57e34bd3b7171121f7881d03aa45b8d56236e41b
iizall <63831837+iizall@users.noreply.github.com>
2022-10-05 21:59:16 +0700

    Tugas 6 PHP

---
+++ navbar.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-expand-lg">
+            <div class="container  is-sticky">
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <a href="index.php" class="navbar-brand mx-auto mx-lg-0">iizall</a>
+
+                <div class="d-flex align-items-center d-lg-none">
+                    <i class="navbar-icon bi-telephone-plus me-3"></i>
+                    <a class="custom-btn btn" href="#section_5">
+                        0856-5858-1358
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-lg-5">
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="index.php?hal=home">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="index.php?hal=about">About</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="index.php?hal=study">Study</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="index.php?hal=tasks">Tasks</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="index.php?hal=portfolio">Portfolio</a>
+                        </li>
+                    </ul>
+
+                    <div class="d-lg-flex align-items-center d-none ms-auto">
+                        <i class="navbar-icon bi-telephone-plus me-3"></i>
+                        <a class="custom-btn btn" href="#section_5">
+                            0856-5858-1358
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </nav>
